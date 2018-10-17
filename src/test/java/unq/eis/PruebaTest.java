8dd13110b3444dec4b2e5d39d69e2034ae522ac3
AugustoConti <augustoconti10@gmail.com>
2018-10-17 18:03:27 -0300

    add method public in test

--- src/test/java/unq/eis/PruebaTest.java
+++ src/test/java/unq/eis/PruebaTest.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.assertEquals;
 public class PruebaTest {
 
     @Test
-    void test_suma() {
+    public void test_suma() {
         Prueba p = new Prueba();
         assertEquals(3, p.sumar(1, 2));
     }
 
     @Test
-    void test_resta() {
+    public void test_resta() {
         Prueba p = new Prueba();
         assertEquals(3, p.restar(5, 2));
     }
