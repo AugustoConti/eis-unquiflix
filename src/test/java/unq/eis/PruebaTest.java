fd6fc12fa4f33822d9dde80a3d3cb24e24908ae5
Augusto <augustoconti10@gmail.com>
2018-10-17 16:11:50 -0300

    add test resta

--- src/test/java/unq/eis/PruebaTest.java
+++ src/test/java/unq/eis/PruebaTest.java
@@ -7,8 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PruebaTest {
 
     @Test
-    void test_uno() {
+    void test_suma() {
         Prueba p = new Prueba();
         assertEquals(3, p.sumar(1, 2));
+    }
+
+    @Test
+    void test_resta() {
+        Prueba p = new Prueba();
+        assertEquals(3, p.restar(5, 2));
     }
 }
