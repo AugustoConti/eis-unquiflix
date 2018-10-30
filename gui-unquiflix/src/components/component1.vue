388c8cfff8260f5d6a8cd4c0aeb36e91750f84ec
facundo <facundo@vigo>
2018-10-30 15:33:05 -0300

    se agrega la funcion filter y se arregla el problema de bindeo

--- gui-unquiflix/src/components/component1.vue
+++ gui-unquiflix/src/components/component1.vue
@@ -1,5 +1,6 @@
 <template>
-    <div >
+    <div class="container-fluid">
+        <!-- <input type="search" class="form-control" v-model="peliculasearch" placeholder="Buscar.."> -->
         <nav id="barra-principal" class="navbar navbar-dark bg-dark sticky-top">
       
       <ul class="nav">
@@ -8,32 +9,55 @@
         </li>
         </ul>
         <div class="div-select">
-          <select class="custom-select" name="" id="">
+          <select class="custom-select" v-model="peliculasearch" name="" id="">
             <option value="" disabled selected hidden>Elegir la categoria...</option>
-            <option value="">Terror</option>
-            <option value="">Romantica</option>
+            <option >Terror</option>
+            <option >Romantica</option>
+            <option value=""> todas</option>  
           </select>
         </div>
       </nav>
         
+        <!-- <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title"></h5>
+                <p class="card-text">some quick example text to build.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
 
+            </div>
+        </div> -->
 
       
-      <!-- <img width="100" :src="image" :alt="title" /> -->
+      <!-- <img width="100" :src="image" :alt="title" />  -->
       <!-- <h2>{{title}}</h2> -->
-      <ul>
-          <li  v-for="pelicula in peliculas" :key="pelicula.id">{{ pelicula.nombre }} - {{ pelicula.categoria }}</li>
+      <div class="card-deck" style="margin: 1rem 1rem"></div>
+      <div class="card" v-for="pelicula in peliculaFilter" :key="pelicula.id" >{{ pelicula.nombre }} - {{ pelicula.categoria }}</div>
+      <div class="card-body"> </div>
+      <!-- <ul>
+          <li class="card-title" v-for="pelicula in peliculaFilter" :key="pelicula.id">{{ pelicula.nombre }} - {{ pelicula.categoria }}</li>
             
-      </ul>
+      </ul> -->
     </div>
 
 </template>
 
 <script>
     export default {
+        computed: {
+            peliculaFilter: function(){
+                var peli=this.peliculasearch
+                return this.peliculas.filter(function (elemnt){
+                return elemnt.categoria.toLowerCase().indexOf(peli.toLowerCase()) !== -1
+                })
+
+            }
+        },
+
         name: "component1",
         data() {
             return {
+                peliculasearch: "",
                 peliculas: [
                 {
                  id: 1,           
